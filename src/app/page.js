52924d426489db09924f6af6205764fca6bd0beb
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2024-03-13 23:46:09 +0600

    service section start

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
-"use client"
-import '@fontsource/russo-one';
+"use client";
+import "@fontsource/russo-one";
 import React from "react";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Service from "@/components/Service";
 
 export default function Home() {
   return (
     <>
-      <Header/>
-      <Hero/>
+      <Header />
+      <Hero />
+      <Service/>
     </>
   );
 }
---
+++ src/components/Service.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <section className="bg-[#050709] w-full py-20">
+      <div className="container mx-auto">
+        <h1 className="text_gradient text-center text-4xl font-bold">My Quality Services</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Service;
